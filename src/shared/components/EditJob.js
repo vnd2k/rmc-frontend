d0cc8e5a537546d4d9e7d09dbf7689e05a36e853
Van Ngoc Dat <vndat74@gmail.com>
2022-07-07 23:42:43 +0700

    commit

--- src/shared/components/EditJob.js
+++ src/shared/components/EditJob.js
@@ -152,6 +152,7 @@ function EditJob() {
                   <a
                     href={item.cvUrl}
                     target={"_blank"}
+                    rel={"noopener"}
                     className={classes.ratingItem}
                   >
                     <li className={classes.itemSearch} key={item.id}>
